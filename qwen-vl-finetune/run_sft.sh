0f5a761ed508e32bff168179e363de316a256d51
Ubuntu <ubuntu@ip-172-31-94-65.ec2.internal>
2025-08-14 05:44:42 +0000

    install python3.12-dev

--- qwen-vl-finetune/run_sft.sh
+++ qwen-vl-finetune/run_sft.sh
@@ -1,5 +1,11 @@
 source /opt/pytorch/bin/activate
 
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+
+sudo apt install -y ffmpeg
+sudo apt install -y python3.12-dev
+
 pip install -r requirements.txt
 
 huggingface-cli download --repo-type dataset --resume-download lmms-lab/LLaVA-NeXT-Data --include llava_next_raw_format/ --local-dir LLaVA-NeXT-Data
@@ -9,6 +15,6 @@ for file in *.tar.gz; do
     tar -xzf "$file"
 done
 
-sh ./script/sft.sh
-sh ./script/sft_7b.sh
-sh ./script/sft_32b.sh
+sh ./scripts/sft.sh
+sh ./scripts/sft_7b.sh
+sh ./scripts/sft_32b.sh
